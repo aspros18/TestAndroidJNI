134e8eb747db1de56cfc241a39a3390851d717f5
aspros18 <aimar18@126.com>
2018-04-21 12:44:33 +0800

    添加log打印

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,26 +7,41 @@
 #include <hash_map>
 #include <hash_set>
 
-extern "C"
-{
+#include <android/log.h>
+#define LOG_TAG    "TestAndroidJNI"
 
-    JNIEXPORT jstring JNICALL Java_com_aspros_testandroidjni_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
-        std::string hello = "Hello from C++";
-        return env->NewStringUTF(hello.c_str());
-    }
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-    JNIEXPORT jstring JNICALL Java_com_aspros_testandroidjni_MainActivity_testVector(JNIEnv *env, jobject /* this */){
-        std::string strTest;
-        int number;
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG, __VA_ARGS__)
+#define LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG, __VA_ARGS__)
+#define LOGF(...)  __android_log_print(ANDROID_LOG_FATAL,LOG_TAG, __VA_ARGS__)
 
-        std::vector<std::string> vector;
-        vector.push_back("last world");
 
-        number = vector.capacity();
-        strTest = vector[number-1];
-
-        return env->NewStringUTF(strTest.c_str());
-    }
+JNIEXPORT jstring JNICALL Java_com_aspros_testandroidjni_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
 }
 
+JNIEXPORT jstring JNICALL Java_com_aspros_testandroidjni_MainActivity_testVector(JNIEnv *env, jobject /* this */){
+    std::string strTest;
+    int number;
+
+    std::vector<std::string> vector;
+    vector.push_back("last world");
+
+    number = vector.capacity();
+    LOGE("number = %d", number);
+    strTest = vector[number-1];
+
+    return env->NewStringUTF(strTest.c_str());
+}
+
+
+#ifdef __cplusplus
+}
+#endif
 
