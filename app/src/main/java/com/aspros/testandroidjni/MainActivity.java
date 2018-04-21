6255bfc9e3065c600aa9df3611b2641cb12b037c
aspros18 <aimar18@126.com>
2018-04-21 12:03:40 +0800

    first commit

---
+++ app/src/main/java/com/aspros/testandroidjni/MainActivity.java
@@ -0,0 +1,42 @@
+package com.aspros.testandroidjni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                TextView tv = (TextView) findViewById(R.id.sample_text);
+                tv.setText(stringFromJNI());
+                break;
+        }
+    }
+}
